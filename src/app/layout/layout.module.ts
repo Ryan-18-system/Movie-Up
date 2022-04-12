8cef64c84fde83d0aa213c04c706c3cc8fe83304
Renata Albuquerque <renataalbq1@gmail.com>
2022-04-12 14:44:33 -0300

    home component

--- src/app/layout/layout.module.ts
+++ src/app/layout/layout.module.ts
@@ -1,3 +1,4 @@
+import { HomeComponent } from './home/home.component';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FooterComponent } from './footer/footer.component';
@@ -8,15 +9,16 @@ import {MenuComponent} from "./menu/menu.component";
 import {MatButtonModule} from "@angular/material/button";
 
 
-
 @NgModule({
     declarations: [
         FooterComponent,
-        MenuComponent
+        MenuComponent,
+        HomeComponent
     ],
     exports: [
         FooterComponent,
-        MenuComponent
+        MenuComponent,
+        HomeComponent
     ],
     imports: [
         CommonModule,
