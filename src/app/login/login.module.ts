d4c582b4d9387d96e470cb4fae1a7ae1f45cb1e2
Ryan Nóbrega Brandão <ryanbrandao18@gmail.com>
2022-04-12 19:39:31 -0300

    alterações gerais

--- src/app/login/login.module.ts
+++ src/app/login/login.module.ts
@@ -1,6 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { LoginComponent } from './login/login.component';
+import {MatInputModule} from "@angular/material/input";
+import {ReactiveFormsModule} from "@angular/forms";
+import {MatIconModule} from "@angular/material/icon";
+import {FlexLayoutModule} from "@angular/flex-layout";
+import {MatButtonModule} from "@angular/material/button";
 
 
 
@@ -8,8 +13,16 @@ import { LoginComponent } from './login/login.component';
   declarations: [
     LoginComponent
   ],
+  exports: [
+    LoginComponent
+  ],
   imports: [
-    CommonModule
+    CommonModule,
+    MatInputModule,
+    ReactiveFormsModule,
+    MatIconModule,
+    FlexLayoutModule,
+    MatButtonModule
   ]
 })
 export class LoginModule { }
