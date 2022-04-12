d57f4fab51667f64d3a9fafb84a87796e5fe2c2c
Ryan Nóbrega Brandão <ryanbrandao18@gmail.com>
2022-04-11 21:46:45 -0300

    adiconando matbuttonmodule

--- src/app/layout/layout.module.ts
+++ src/app/layout/layout.module.ts
@@ -4,21 +4,26 @@ import { FooterComponent } from './footer/footer.component';
 import {MatToolbarModule} from "@angular/material/toolbar";
 import {MatIconModule} from "@angular/material/icon";
 import {MatCardModule} from "@angular/material/card";
+import {MenuComponent} from "./menu/menu.component";
+import {MatButtonModule} from "@angular/material/button";
 
 
 
 @NgModule({
     declarations: [
-        FooterComponent
+        FooterComponent,
+        MenuComponent
     ],
     exports: [
-        FooterComponent
+        FooterComponent,
+        MenuComponent
     ],
     imports: [
         CommonModule,
         MatToolbarModule,
         MatIconModule,
-        MatCardModule
+        MatCardModule,
+        MatButtonModule
     ]
 })
 export class LayoutModule { }
