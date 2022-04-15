8d52ac1688733e00a1e64dc6fca57d4c98ab2e51
Ryan Nóbrega Brandão <ryanbrandao18@gmail.com>
2022-04-15 10:32:39 -0300

    adicionando componentes serie

---
+++ src/app/serie/listar-series/listar-series.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-listar-series',
+  templateUrl: './listar-series.component.html',
+  styleUrls: ['./listar-series.component.css']
+})
+export class ListarSeriesComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
