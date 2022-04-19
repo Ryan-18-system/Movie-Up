408643efc24d447fb28f9d388037ecc221e20c71
Ryan Nóbrega Brandão <ryanbrandao18@gmail.com>
2022-04-19 18:31:16 -0300

    criando serviço e model de detalhe

---
+++ src/app/shared/model/detalhe.ts
@@ -0,0 +1,10 @@
+export class Detalhe{
+  titulo!: string;
+  imagem!: string;
+  dataLancamento!: number;
+  sinopse!:string;
+  genero!: string;
+  diretor!: string;
+  atores!: Array<string>;
+  duracao!: number;
+}
