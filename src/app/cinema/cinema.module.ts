570efdf011166569125d51a1b645b32d03695cb2
Renata Albuquerque <renataalbq1@gmail.com>
2022-05-27 10:52:58 -0300

    estilizando selects página cinema

--- src/app/cinema/cinema.module.ts
+++ src/app/cinema/cinema.module.ts
@@ -1,9 +1,13 @@
+import { MatSelectModule } from '@angular/material/select';
+import { FormsModule } from '@angular/forms';
+import { ReactiveFormsModule } from '@angular/forms';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { BuscarCinemaComponent } from './buscar-cinema/buscar-cinema.component';
 import {MatMenuModule} from "@angular/material/menu";
 import {MatFormFieldModule} from "@angular/material/form-field";
 import {MatCardModule} from "@angular/material/card";
+import { MatOptionModule } from '@angular/material/core';
 
 
 
@@ -15,7 +19,11 @@ import {MatCardModule} from "@angular/material/card";
     CommonModule,
     MatMenuModule,
     MatFormFieldModule,
-    MatCardModule
+    MatCardModule,
+    ReactiveFormsModule,
+    FormsModule,
+    MatSelectModule,
+    MatOptionModule
   ],
   exports:[
     BuscarCinemaComponent
