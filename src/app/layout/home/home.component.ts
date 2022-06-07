68370b839bcb1eaae66147f7eb4a57ec4a918b10
Renata Albuquerque <renataalbq1@gmail.com>
2022-06-07 06:06:30 -0300

    add botão favoritar

--- src/app/layout/home/home.component.ts
+++ src/app/layout/home/home.component.ts
@@ -1,6 +1,9 @@
+import { FavoritosService } from './../../shared/service/favoritos.service';
 import { Component, OnInit } from '@angular/core';
 import { Filme } from 'src/app/shared/model/Filme';
 import { FilmeService } from 'src/app/shared/service/filmes.service';
+import { MenssageService } from 'src/app/shared/service/menssage.service';
+import { Favoritos } from 'src/app/shared/model/favoritos';
 
 @Component({
   selector: 'app-home',
@@ -10,6 +13,8 @@ import { FilmeService } from 'src/app/shared/service/filmes.service';
 export class HomeComponent implements OnInit {
 
   filmes: Array<Filme> = [];
+  favoritosAdd: Favoritos;
+
 
   favoritos = [
     {img: 'https://www.themoviedb.org/t/p/original/rvtdN5XkWAfGX6xDuPL6yYS2seK.jpg'},
@@ -17,12 +22,24 @@ export class HomeComponent implements OnInit {
     {img: 'https://www.themoviedb.org/t/p/original/xvIAeAyXakMHPOgU7URp1kqKQZI.jpg'},
   ]
 
-  constructor(private filmeService: FilmeService) { }
+  constructor(private filmeService: FilmeService, private favoritosService: FavoritosService, private menssageService: MenssageService) {
+    this.favoritosAdd = new Favoritos;
+
+  }
 
   ngOnInit(): void {
     this.filmeService.listarEmalta().subscribe(
       filmes => this.filmes = filmes
     );
+  }
+
+  inserirFav(id: Filme["id"]): void {
+    this.favoritosService.inserir(id).subscribe(
+      favoritosAdd => this.menssageService.success("Filme adicionado aos favoritos")
+      )
+    this.favoritosAdd = new Favoritos()
+    console.log(this.favoritosAdd)
+
   }
 
 }
