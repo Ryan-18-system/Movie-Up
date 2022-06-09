d4d0b7ffcb6288c4791cf4f0fecced907ba89fd1
Renata Albuquerque <renataalbq1@gmail.com>
2022-06-09 19:48:50 -0300

    deletando favoritos

--- src/app/layout/home/home.component.ts
+++ src/app/layout/home/home.component.ts
@@ -1,9 +1,6 @@
-import { FavoritosService } from './../../shared/service/favoritos.service';
 import { Component, OnInit } from '@angular/core';
 import { Filme } from 'src/app/shared/model/Filme';
 import { FilmeService } from 'src/app/shared/service/filmes.service';
-import { MenssageService } from 'src/app/shared/service/menssage.service';
-import { Favoritos } from 'src/app/shared/model/favoritos';
 
 @Component({
   selector: 'app-home',
@@ -13,33 +10,24 @@ import { Favoritos } from 'src/app/shared/model/favoritos';
 export class HomeComponent implements OnInit {
 
   filmes: Array<Filme> = [];
-  favoritosAdd: Favoritos;
-
 
   favoritos = [
-    {img: 'https://www.themoviedb.org/t/p/original/rvtdN5XkWAfGX6xDuPL6yYS2seK.jpg'},
-    {img: 'https://www.themoviedb.org/t/p/original/iQFcwSGbZXMkeyKrxbPnwnRo5fl.jpg'},
-    {img: 'https://www.themoviedb.org/t/p/original/xvIAeAyXakMHPOgU7URp1kqKQZI.jpg'},
+    {imagem: 'https://www.themoviedb.org/t/p/original/rvtdN5XkWAfGX6xDuPL6yYS2seK.jpg'},
+    {imagem: 'https://www.themoviedb.org/t/p/original/iQFcwSGbZXMkeyKrxbPnwnRo5fl.jpg'},
+    {imagem: 'https://www.themoviedb.org/t/p/original/xvIAeAyXakMHPOgU7URp1kqKQZI.jpg'},
   ]
 
-  constructor(private filmeService: FilmeService, private favoritosService: FavoritosService, private menssageService: MenssageService) {
-    this.favoritosAdd = new Favoritos;
-
+  constructor(private filmeService: FilmeService) {
   }
 
   ngOnInit(): void {
+    this.filmeService.listarFavoritos().subscribe(
+      filmes => this.filmes = filmes
+    )
+
     this.filmeService.listarEmalta().subscribe(
       filmes => this.filmes = filmes
     );
-  }
-
-  inserirFav(id: Filme["id"]): void {
-    this.favoritosService.inserir(id).subscribe(
-      favoritosAdd => this.menssageService.success("Filme adicionado aos favoritos")
-      )
-    this.favoritosAdd = new Favoritos()
-    console.log(this.favoritosAdd)
-
   }
 
 }
