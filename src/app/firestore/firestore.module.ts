2531c7704ae4f9df6d0a7bc32827311623b72150
Ryan Nóbrega Brandão <ryanbrandao18@gmail.com>
2022-06-27 17:49:20 -0300

    adicionando cadastro de usuário com firebase

--- src/app/firestore/firestore.module.ts
+++ src/app/firestore/firestore.module.ts
@@ -1,19 +1,47 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+
 import {AngularFireModule} from '@angular/fire';
 import {FirebaseConfig} from 'firebase.config';
 import {AngularFirestoreModule} from '@angular/fire/firestore';
 import {AngularFireAuthModule} from '@angular/fire/auth';
 import {AngularFireDatabaseModule} from '@angular/fire/database';
+import { LoginComponent } from './login/login.component';
+import { CadastroComponent } from './cadastro/cadastro.component';
+import {MatInputModule} from "@angular/material/input";
+import {FormsModule, ReactiveFormsModule} from "@angular/forms";
+import {MatIconModule} from "@angular/material/icon";
+import {MatCardModule} from "@angular/material/card";
+import {FlexLayoutModule} from "@angular/flex-layout";
+import {MatButtonModule} from "@angular/material/button";
+
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    LoginComponent,
+    CadastroComponent
+  ],
   imports: [
     CommonModule,
     AngularFireModule.initializeApp(FirebaseConfig),
     AngularFirestoreModule,
     AngularFireAuthModule,
-    AngularFireDatabaseModule
+    AngularFireDatabaseModule,
+    MatInputModule,
+    ReactiveFormsModule,
+    MatIconModule,
+    FormsModule,
+    MatCardModule,
+    FlexLayoutModule,
+    MatButtonModule,
+
+
+  ],
+  exports: [
+    LoginComponent,
+    CadastroComponent
   ]
 })
-export class FirestoreModule { }
+export class FirestoreModule {
+
+}
