6a0e0028690435efb143ff05b4caefbbd4018dc3
Renata Albuquerque <renataalbq1@gmail.com>
2022-06-29 08:43:19 -0300

    botão 'saiba mais' da pagina de busca levando para detalhes

--- src/app/shared/service/buscar-filme.service.ts
+++ src/app/shared/service/buscar-filme.service.ts
@@ -1,3 +1,4 @@
+import { Serie } from 'src/app/shared/model/serie';
 import { Injectable } from '@angular/core';
 import {Observable} from "rxjs";
 import {Filme} from "../model/Filme";
@@ -8,6 +9,8 @@ import {HttpClient} from "@angular/common/http";
 })
 export class BuscarFilmeService {
   URL_FILME = 'http://localhost:8082/filmes/titulo'
+  URL_SERIE = 'http://localhost:8082/series/titulo'
+
   constructor(private  httpClient: HttpClient) { }
 
   buscarFilme(filme:string): Observable<Filme[]> {
@@ -18,5 +21,12 @@ export class BuscarFilmeService {
 
   pesquisar(id:number): Observable<Filme> {
     return this.httpClient.get<Filme>(`${this.URL_FILME}?/${id}`);
+  }
+
+
+  buscarSerie(serie:string): Observable<Serie[]> {
+    const serieCorrigida = serie.toLowerCase()
+    return this.httpClient.get<Serie[]>(`${this.URL_SERIE}?titulo=${serieCorrigida}`);
+
   }
 }
