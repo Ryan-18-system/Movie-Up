f6bdba74987915473ac062f5c361916101c69d6d
Renata Albuquerque <renataalbq1@gmail.com>
2022-06-29 07:52:38 -0300

    funcionalidades da pagina detalhes

--- src/app/detalhe/detalhe.component.ts
+++ src/app/detalhe/detalhe.component.ts
@@ -1,11 +1,10 @@
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 import { MensagemService } from './../shared/service/mensagem.service';
 import { CriticasService } from 'src/app/shared/service/criticas.service';
 import { FilmeService } from 'src/app/shared/service/filmes.service';
 import { Component, OnInit } from '@angular/core';
 import { Filme } from '../shared/model/Filme';
 import { Critica } from '../shared/model/criticas';
-import { Router } from '@angular/router';
 
 
 
@@ -19,12 +18,13 @@ export class DetalheComponent implements OnInit {
   filme: Filme;
   filmes: Array<Filme> = [];
   critica: Array<Critica> = [];
-  filmeManipulado = new Filme();
+  criticas: Critica;
 
   constructor(private filmeService: FilmeService,
     private criticaService: CriticasService,
     private mensagemService: MensagemService,
     private roteador: Router, private rotaAtual: ActivatedRoute) {
+      this.criticas = new Critica();
       this.filme = new Filme();
       if (this.rotaAtual.snapshot.paramMap.has('id')){
         const idParaFilme = this.rotaAtual.snapshot.paramMap.get('id');
@@ -51,6 +51,18 @@ export class DetalheComponent implements OnInit {
       critica =>
       {this.critica = critica
         console.log(critica)}
+    )
+  }
+
+  remover(criticas: Critica): void {
+    this.criticaService.remover(criticas).subscribe(
+      resposta => {
+        const indxCriticaARemover = this.critica.findIndex(u => u.mensagem === criticas.mensagem);
+        if (indxCriticaARemover > -1) {
+          this.critica.splice(indxCriticaARemover, 1);
+          this.mensagemService.success("Crítica removida com sucesso")
+        }
+      }
     )
   }
 
